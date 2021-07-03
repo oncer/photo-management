35fe8acac19352dd4a5ca674540845b840fa2407
Simon Parzer <simon.parzer@gmail.com>
2021-07-03 14:03:48 +0200

    do not resize if file size is <2MB

--- resize.sh
+++ resize.sh
@@ -2,5 +2,10 @@
 shopt -s nocaseglob
 for i in *.jpg
 do
+	size=$(stat -c %s "$i")
+	if (($size < 2 * 1024*1024)); then
+	   continue
+   	fi
+	echo "Resizing $i..."
 	magick "$i" -scale 1920x1920\> -auto-orient -quality 92 "$i"
 done
